312e00a7b159c6d5f4335175a8b75c0d148ca2fb
Pao2PAO <clavel.paolo.esporas@gmail.com>
2025-10-03 07:31:15 +0800

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,35 @@
+// Book.java
+public class Book {
+    private String title;
+    private String author;
+    private int yearPublished;
+    private int currentPage;
+
+    public Book(String title, String author, int yearPublished) {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+        this.currentPage = 0;
+    }
+
+    public void displayDetails() {
+        System.out.println("Title: " + title + ", Author: " + author +
+                ", Year: " + yearPublished + ", Current Page: " + currentPage);
+    }
+
+    public void changePage(int page) {
+        this.currentPage = page;
+    }
+
+    public void changeAuthor(String newAuthor) {
+        this.author = newAuthor;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+}
