48da1092b68f3176e041d419cd5a61daa32a9659
Pao2PAO <clavel.paolo.esporas@gmail.com>
2025-10-28 06:23:14 +0800

    Create SmartHomeController.java

---
+++ SmartHomeController.java
@@ -0,0 +1,25 @@
+// Main program: SmartHomeMain.java
+
+// SmartHomeController class
+class SmartHomeController {
+    private Device[] devices;
+
+   
+    public SmartHomeController(Device[] devices) {
+        this.devices = devices;
+    }
+
+    // Turn the devices on
+    public void turnAllOn() {
+        for (Device device : devices) {
+            device.turnOn();
+        }
+    }
+
+    // Turn the devices off
+    public void turnAllOff() {
+        for (Device device : devices) {
+            device.turnOff();
+        }
+    }
+}
